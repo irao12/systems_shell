2d22f697f38e90c6cea53edc2db69b4fc86e090a
irao12 <71599573+irao12@users.noreply.github.com>
2020-12-16 01:08:38 -0500

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,10 +1,9 @@
-#include <sys/stat.h>
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-int count_args( char * line );
+int count_args( char * line, char delimeter );
 
-char ** parse_args ( char * line, int args );
+char ** parse_args ( char * line, int args, char *delimeter );
 
 void remover(char* buffer);
 
@@ -13,4 +12,6 @@ int count_cmds(char * cmd);
 char ** parse_cmds (char* cmd, int num);
 
 char * remove_spaces(char *s);
+
+char** redirect (char** arguments, int num_of_args);
 #endif
