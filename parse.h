4743da6d110f5ee4612d2042764154a3b1daa4ac
irao12 <71599573+irao12@users.noreply.github.com>
2020-12-19 19:02:46 -0500

    Update parse.h

--- parse.h
+++ parse.h
@@ -5,9 +5,9 @@ int count_cmds(char * cmd_line);
 
 char ** parse_cmds (char* cmd, int num);
 
-int count_args ( char * line, char delimeter );
+int count_args ( char * line );
 
-char ** parse_args ( char * line, int args, char *delimeter );
+char ** parse_args ( char * line, int args);
 
 void remover(char* buffer);
 
